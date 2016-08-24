283218237e23273ace67cbd5a1495014d07e619e
Dylan Graham <dylan.graham@gmail.com>
2016-08-24 11:13:18 +1000

    Clean up

--- Mendels.First.Law.py
+++ Mendels.First.Law.py
@@ -1,7 +1,15 @@
 from fractions import Fraction as Fr
 
+data = {}
 
-def pick(data, x):
+
+def reset_data():
+    global data
+    data = {'a': 2, 'b': 2, 'c': 2}
+
+
+def pick(x):
+    global data
     if data[x] > 0:
         data[x] -= 1
         return True
@@ -10,67 +18,67 @@ def pick(data, x):
 
 
 def prob(x):
-    return Fr(x, sum(data.values()))
+    return Fr(data[x], sum(data.values()))
 
 print('Probability of 2 x a')
-data = {'a': 2, 'b': 2, 'c': 2}
-p1 = prob(data['a'])
-pick(data, 'a')
-p2 = prob(data['a'])
+reset_data()
+p1 = prob('a')
+pick('a')
+p2 = prob('a')
 print(p1 * p2)
 
 print('Probability of 2 x b')
-data = {'a': 2, 'b': 2, 'c': 2}
-p1 = prob(data['b'])
-pick(data, 'b')
-p2 = prob(data['b'])
+reset_data()
+p1 = prob('b')
+pick('b')
+p2 = prob('b')
 print(p1 * p2)
 
 print('Probability of 2 x c')
-data = {'a': 2, 'b': 2, 'c': 2}
-p1 = prob(data['c'])
-pick(data, 'c')
-p2 = prob(data['c'])
+reset_data()
+p1 = prob('c')
+pick('c')
+p2 = prob('c')
 print(p1 * p2)
 
 print('Probability of 1 x a, 1 x b')
-data = {'a': 2, 'b': 2, 'c': 2}
-p1 = prob(data['a'])
-pick(data, 'a')
-p2 = prob(data['b'])
+reset_data()
+p1 = prob('a')
+pick('a')
+p2 = prob('b')
 print(p1 * p2)
 
 print('Probability of 1 x a, 1 x c')
-data = {'a': 2, 'b': 2, 'c': 2}
-p1 = prob(data['a'])
-pick(data, 'a')
-p2 = prob(data['c'])
+reset_data()
+p1 = prob('a')
+pick('a')
+p2 = prob('c')
 print(p1 * p2)
 
 print('Probability of 1 x b, 1 x a')
-data = {'a': 2, 'b': 2, 'c': 2}
-p1 = prob(data['b'])
-pick(data, 'b')
-p2 = prob(data['a'])
+reset_data()
+p1 = prob('b')
+pick('b')
+p2 = prob('a')
 print(p1 * p2)
 
 print('Probability of 1 x b, 1 x c')
-data = {'a': 2, 'b': 2, 'c': 2}
-p1 = prob(data['b'])
-pick(data, 'b')
-p2 = prob(data['c'])
+reset_data()
+p1 = prob('b')
+pick('b')
+p2 = prob('c')
 print(p1 * p2)
 
 print('Probability of 1 x c, 1 x a')
-data = {'a': 2, 'b': 2, 'c': 2}
-p1 = prob(data['c'])
-pick(data, 'c')
-p2 = prob(data['a'])
+reset_data()
+p1 = prob('c')
+pick('c')
+p2 = prob('a')
 print(p1 * p2)
 
 print('Probability of 1 x c, 1 x b')
-data = {'a': 2, 'b': 2, 'c': 2}
-p1 = prob(data['c'])
-pick(data, 'c')
-p2 = prob(data['b'])
+reset_data()
+p1 = prob('c')
+pick('c')
+p2 = prob('b')
 print(p1 * p2)
